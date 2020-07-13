7741bbcb2d461e220e08fa8201a9eff034221add
Elioenai Ferrari <elioenaiferrari@gmail.com>
2020-07-12 22:16:43 -0300

    ibm-watson

--- src/config/app.js
+++ src/config/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import watsonRoutes from '../routes/watson';
 
 const app = express();
 
+app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(watsonRoutes);

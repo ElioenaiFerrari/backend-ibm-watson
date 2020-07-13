2f32eeabf375981fcf58f7ee92416a15457257c2
Elioenai Ferrari <elioenaiferrari@gmail.com>
2020-07-12 22:28:03 -0300

    ibm-watson

--- src/controllers/watson.js
+++ src/controllers/watson.js
@@ -1,24 +1,13 @@
 import Assistant from '../services/assistant';
 import watsonConfig from '../config/watson';
 
-const {
-  WORKSPACE_ID,
-  ASSISTANT_APIKEY,
-  ASSISTANT_URL,
-} = watsonConfig;
+const { WORKSPACE_ID, ASSISTANT_APIKEY, ASSISTANT_URL } = watsonConfig;
 
-const assistant = new Assistant(
-  WORKSPACE_ID,
-  ASSISTANT_APIKEY,
-  ASSISTANT_URL
-);
+const assistant = new Assistant(WORKSPACE_ID, ASSISTANT_APIKEY, ASSISTANT_URL);
 
 const WatsonController = {
   async chat(req, res) {
-    const {
-      text,
-      context = {}
-    } = req.body;
+    const { text } = req.body;
 
     const params = {
       input: {
@@ -35,7 +24,7 @@ const WatsonController = {
       return res.json(response);
     } catch (error) {
       console.log(error);
-      return res.status(500).json(error)
+      return res.status(500).json(error);
     }
   },
 };
